eea5a3ad04e6b8cc32a4d6bf4037af07be9661e6
Robert Angerer <robert.angerer@outlook.com>
2023-04-15 00:03:57 +0200

    wip: get element and border it yellow with after delay

--- studius.js
+++ studius.js
@@ -1,2 +1,19 @@
 document.body.style.border = "5px solid red";
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+(async () => {
+    console.log('Start');
+    await delay(5000); // Wait for 5 seconds (2000 milliseconds)
+    let container = document.getElementsByClassName("container mx-auto flex h-full max-w-7xl flex-col p-4 md:px-16")
+    console.log(container)
+    let array = Array.from(container)
+    console.log(array)
+    container.item(0).style.border = "5px solid yellow"
+    console.log('Execution resumes after 2 seconds');
+})();
+
+
+
 
